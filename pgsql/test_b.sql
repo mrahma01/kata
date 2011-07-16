92826b3f97c9a012044a7dc7b7d509f9be17c146
Muhammad Rahman <muhammmad.rahman@aemedia.com>
2011-07-16 18:14:52 +0100

    day in month commented help

--- pgsql/test_b.sql
+++ pgsql/test_b.sql
@@ -1,10 +1,14 @@
-CREATE OR REPLACE FUNCTION day_in_month(month int, int DEFAULT NULL) RETURNS int AS $$
+CREATE OR REPLACE FUNCTION day_in_month(month int, int DEFAULT NULL) 
+                                                   RETURNS int AS $$
 DECLARE
     year integer;
 BEGIN
     IF $2 is NULL THEN
-        year := select extract(year from now());
+        year := 2011;
     END IF;        
     return month+year;
 END;
 $$ LANGUAGE plpgsql;
+--select extract(year from now());
+--select to_char(current_date, 'day');
+--select current_date + s.a as dates from generate_series(0,14,7) as s(a);
