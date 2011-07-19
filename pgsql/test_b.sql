f9dd2597c175d944f1cdbfa592476c9500a31c7c
Muhammad Rahman <muhammad.rahman@aemedia.com>
2011-07-19 12:30:17 +0100

    day_in_month final

--- pgsql/test_b.sql
+++ pgsql/test_b.sql
@@ -1,14 +1,17 @@
-CREATE OR REPLACE FUNCTION day_in_month(month int, int DEFAULT NULL) 
+CREATE OR REPLACE FUNCTION day_in_month(month int, y int DEFAULT NULL) 
                                                    RETURNS char AS $$
 DECLARE
-    year integer;
+    today varchar;
 BEGIN
-    IF $2 is NULL THEN
-        year := 2011;
+    IF y is NULL THEN
+        today := extract(year from now()) || 
+                         '-' || $1 || '-' || 
+                          extract(day from now());
+    ELSE 
+        today := y || '-' || $1 || '-' || extract(day from now());
+        RAISE NOTICE '%', today;
+
     END IF;        
-    return to_char(to_date('2011-07-18', 'yyyy-mm-dd'), 'Day, dd Month, yyyy');
+    return to_char(to_date(today, 'yyyy-mm-dd'), 'Day, dd Month, yyyy');
 END;
 $$ LANGUAGE plpgsql;
---select extract(year from now());
---select current_date + s.a as dates from generate_series(0,14,7) as s(a);
---select to_char(current_date, 'Day, d Month, yyyy');
