3d5a71316af20c61dc93681dc4baa18418ff88ba
Muhammad Rahman <muhammad.rahman@aemedia.com>
2011-07-19 17:22:08 +0100

    time delta diff unfinished

--- pgsql/test_b.sql
+++ pgsql/test_b.sql
@@ -12,6 +12,19 @@ BEGIN
         RAISE NOTICE '%', today;
 
     END IF;        
-    return to_char(to_date(today, 'yyyy-mm-dd'), 'Day, dd Month, yyyy');
+    return to_char(to_date(today, 'yyyy-mm-dd'), 'Day, dd Month yyyy');
 END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION time_delta_diff(varchar, varchar) RETURNS 
+                                                interval AS $$
+DECLARE
+    a varchar;
+    b varchar;
+BEGIN
+    a := ''''||$2||'''';
+    b := ''''||$1||'''';
+    RAISE NOTICE '%', a;
+    return age(timestamp '2005-02-01 00:20:34' , timestamp '2004-01-02 00:02:34');
+END;    
 $$ LANGUAGE plpgsql;
